d61dd3f9ae00af3ca332c6d2641973bd79225df5
Riya Mehta <mehtariya94133@gmail.com>
2024-01-06 19:10:53 +0530

    ok

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -28,6 +28,7 @@ void quickSort(int arr[], int low, int high)
     {
         int pi = partition(arr, low, high);
         quickSort(arr, low, pi - 1);
+        printf("hello");
         quickSort(arr, pi + 1, high);
     }
 }
